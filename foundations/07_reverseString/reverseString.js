82c9240179bd15f82406aa4d3db30f4dabfc5612
Shaikh Zaib <shaikhzaib04@gmail.com>
2026-06-14 01:06:59 +0530

    Add solution for 07 exercise in foundation/javascript-exercises

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+//   return str.split('').reverse().join("");
 
+let reversedString = "";
+
+for(let i = str.length - 1; i >=0; i--){
+    reversedString += str[i];
+}
+
+return reversedString;
 };
 
 // Do not edit below this line
