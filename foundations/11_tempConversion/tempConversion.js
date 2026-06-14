5c991c6d3cf454feb768e600cd546955a21b0d0a
Shaikh Zaib <shaikhzaib04@gmail.com>
2026-06-14 22:46:50 +0530

    Add solution for 11 exercise in foundation/javascript-exercises

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    const tempInCelsius = (temp - 32) * (5 / 9);
+    let rounded = Number(tempInCelsius.toFixed(1));
+    return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+   const tempInFahrenheit = (temp * (9 / 5)) + 32;
+   let rounded = Number(tempInFahrenheit.toFixed(1));
+   return rounded;
 };
 
 // Do not edit below this line
