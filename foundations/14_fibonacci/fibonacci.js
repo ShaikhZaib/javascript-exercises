875b5ddb7973f98665610a2b0a6e1029f533aa81
Shaikh Zaib <shaikhzaib04@gmail.com>
2026-06-19 01:47:26 +0530

    Add solution for 14 exercise in javaScript-exercise/foundation

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    // if(n < 0) return "OOPS"
+    // if(n == 0 ) return 0;
+    // if(n == 1 || n == 2) return 1;
+
+    // return fibonacci( n - 1) + fibonacci( n - 2);
+
+    let count;
+    if(typeof n !== "number"){
+        count = parseInt(n);
+    } else{
+        count = n;
+    }
+
+    if(count < 0) return "OOPS";
+    if(count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for(let i = 2; i <= count; i++){
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 
 };
 
